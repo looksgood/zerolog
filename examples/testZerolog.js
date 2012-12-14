9414921ef1e415a7e7ec1c3018dd741794019c13
王亮杰 <liangjie.wanglj@alibaba-inc.com>
2012-12-14 15:25:22 +0800

    add example

---
+++ examples/testZerolog.js
@@ -0,0 +1,7 @@
+var ZeroLog = require('../lib/ZeroLog');
+
+var zeroLog = new ZeroLog();
+
+zeroLog.log('info', 'test result: ', {type: 'test', name: 'zerolog'});
+zeroLog.log('this is test log');
+zeroLog.log({level: 'info', msg: 'this is info message'});
