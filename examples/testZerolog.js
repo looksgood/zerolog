96b2e0dcfefb461c7eaf905df68344a9e79824da
王亮杰 <liangjie.wanglj@alibaba-inc.com>
2013-03-18 14:54:23 +0800

    add rollDateFormat

--- examples/testZerolog.js
+++ examples/testZerolog.js
@@ -2,6 +2,11 @@ var ZeroLog = require('../lib/ZeroLog');
 
 var zeroLog = new ZeroLog();
 
-zeroLog.log('info', 'test result: ', {type: 'test', name: 'zerolog'});
-zeroLog.log('this is test log');
-zeroLog.log({level: 'info', msg: 'this is info message'});
+//zeroLog.log('info', 'test result: ', {type: 'test', name: 'zerolog'});
+//zeroLog.log('this is test log');
+//zeroLog.log({level: 'info', msg: 'this is info message', });
+
+log('info', 'test result: ', {type: 'test', name: 'zerolog'})
+function log(){
+  zeroLog.log(arguments);
+}
